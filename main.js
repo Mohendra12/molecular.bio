7d4f159aba678052b6c1bb0acc76d595b055fef8
mohendrathapa418@gmail.com <mohendrathapa418@gmail.com>
2024-01-02 23:18:24 +0530

    update1

--- main.js
+++ main.js
@@ -13,3 +13,17 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+  const coll = document.querySelectorAll('.collapse');
+  coll.forEach(function(item) {
+    item.addEventListener('click', function() {
+      this.classList.toggle('active');
+      const content = this.nextElementSibling;
+      if (content.style.display === 'block') {
+        content.style.display = 'none';
+      } else {
+        content.style.display = 'block';
+      }
+    });
+  });
+});
